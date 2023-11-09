2d5ee02198971a0cc79b5d1cc63ca91785feea8e
OrekiPotato <mojangthesimlord@outlook.com>
2023-11-09 16:51:53 +0800

    Continued with Part 2 (Still not working)

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/MeshManager.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/Sonic/MeshManager.cs
@@ -17,7 +17,8 @@ public class MeshManager : MonoBehaviour
     private MeshFilter meshFilter;
 
     [HideInInspector]
-    public Mesh originalMesh, clonedMesh;
+    public Mesh originalMesh { get; private set; }
+    public Mesh clonedMesh { get; private set; }
 
     public Vector3[] vertices { get; private set; }
     public int[] triangles { get; private set; }
@@ -26,12 +27,14 @@ public class MeshManager : MonoBehaviour
     {
         meshFilter = GetComponent<MeshFilter>();
         originalMesh = meshFilter.sharedMesh;
+
         clonedMesh = new Mesh();
         clonedMesh.name = "clone";
         clonedMesh.vertices = originalMesh.vertices;
         clonedMesh.triangles = originalMesh.triangles;
         clonedMesh.normals = originalMesh.normals;
         clonedMesh.uv = originalMesh.uv;
+
         meshFilter.mesh = clonedMesh;
 
         vertices = clonedMesh.vertices;
